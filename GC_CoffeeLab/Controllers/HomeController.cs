9006016d030da49007bcc2c3818adbb83aa8894f
carterave519 <carterave519@gmail.com>
2018-05-31 09:47:35 -0400

    Lab 23 first commit

--- GC_CoffeeLab/Controllers/HomeController.cs
+++ GC_CoffeeLab/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GC_CoffeeLab.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,28 +12,70 @@ namespace GC_CoffeeLab.Controllers
     {
         public ActionResult Index()
         {
+            CoffeeEntities ORM = new CoffeeEntities();
+
+            ViewBag.Items = ORM.Items.ToList();
+            ViewBag.Users = ORM.Users.ToList();
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult About(Item data)
         {
-            ViewBag.Message = "Your application description page.";
+            CoffeeEntities ORM = new CoffeeEntities();
+            if (ModelState.IsValid)
+            {
+                
+                ORM.Items.Add(data);
+                
+                ORM.SaveChanges();
+                ViewBag.message = $"{data.Description} has been added";
+            }
+            else
+            {
+                ViewBag.message = "Item is not valid, cannot add to DB.";
+            }
 
             return View();
         }
 
-        public ActionResult Contact()
+            
+        }
+
+    //we pass in the ItemID so that we can remove a certain object
+    public ActionResult Contact(int ID)
+    {
+        CoffeeEntities ORM = new CoffeeEntities();
+        //we build this object so that we can make a transaction
+        DbContextTransaction DeleteCustomerTransaction = ORM.Database.BeginTransaction();
+        Item temp = new Item();
+        try
         {
-            ViewBag.Message = "Your contact page.";
+            //we first find the specific item by the items id
+            temp = ORM.Items.Find(ID);
+            ORM.Items.Remove(temp);
+            ORM.SaveChanges();
+            DeleteCustomerTransaction.Commit();
+            //if the remove was successful we commit the transaction
+            ViewBag.Message = $"{temp.Description} was removed";
+        }
+        catch (Exception ex)
+        {
+            //if the remove was unsuccessful then we 
+            //roll back the transaction so no data is lost
+            DeleteCustomerTransaction.Rollback();
+            ViewBag.Message = "Item could not be removed";
 
             return View();
         }
+
+        
+
         //this will go to my registration page
         //1.Link/2.ActionResult/3.View
         public ActionResult Registration()
         {
             
-            return View();
+            // View();
         }
 
         public ActionResult Welcome(string first = "First", string last = "Last", int phone = 111-222-3333, string email = "email@whatever.com", 
